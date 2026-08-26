f55ee25bacd5bd269a3b99280bca2570859d0f1b
Cesar Merino-Soto <sikayax@yahoo.com.ar>
2026-08-26 10:15:47 -0600

    Add rse_data dataset and remove EFA.dimensions dependency

---
+++ R/rse_data.R
@@ -0,0 +1,19 @@
+#' Rosenberg Self-Esteem Scale Example Dataset
+#'
+#' A data frame with 800 observations on 10 items from the
+#' Rosenberg Self-Esteem Scale. Negatively worded items were
+#' reverse scored.
+#'
+#' @format A data frame with 800 rows and 10 variables.
+#'
+#' @source Adapted from the archived package EFA.dimensions v0.1.8.8.
+#'
+#' @references
+#' Rosenberg, M. (1965).
+#' \emph{Society and the Adolescent Self-Image}.
+#' Princeton University Press.
+#'
+#' @examples
+#' head(rse_data)
+#'
+"rse_data"
